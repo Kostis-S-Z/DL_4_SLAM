2c3947386d42d55a8ae2317695ad37b04db96231
Jule <Jule@MacBook-Air.local>
2019-04-27 17:12:04 +0200

    LSTM for more then 1 timestep

--- starter_code/simple_lstm.py
+++ starter_code/simple_lstm.py
@@ -36,7 +36,7 @@ class SimpleLstm:
         # for all training examples compute one hot encoding
         for i, training_example in enumerate(training_data):
             for train_feature in training_example.keys():
-                feature_attribute, feature_value = train_feature.split(":")
+                feature_attribute, feature_value = train_feature.split(":", 1)
                 index_attribute = feature_index_dict[feature_attribute][0]
                 index_value = feature_index_dict[feature_attribute][1][feature_value]
                 index = index_attribute + index_value
@@ -49,13 +49,10 @@ class SimpleLstm:
         shape X_train = (n, one_hot_m)
         shape y_train = (n, )
         """
-        timesteps = 2
-        lstm_layer_size = 10
-        # should be: (batch_size/ num_examples, num_timesteps, num_feats (m))
-        X_train2 = np.zeros((X_train.shape[0], 2, X_train.shape[1]))
-        X_train2[:,1,:] = X_train
-        X_train2[:,0,:] = X_train
-        X_train = X_train2
+        timesteps = 32
+        lstm_layer_size = 100
+
+        X_train =X_train, y_train = data_in_time(X_train, y_train, timesteps)
         #X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
 
         model = Sequential()
@@ -64,6 +61,7 @@ class SimpleLstm:
         # return_sequences = True. We want this because we want to output correct/incorrect for each word, not just
         # at the end of the exercise one correct/incorrect
         model.add(LSTM(lstm_layer_size, return_sequences=False, input_shape = (timesteps, X_train.shape[2])))
+        #model.add(LSTM(lstm_layer_size, return_sequences=True, input_shape=(timesteps, lstm_layer_size)))
         # This function adds an independent layer for each time step in the recurrent model. So, for instance, if we
         # have 10 time steps in a model, a TimeDistributed layer operating on a Dense layer would produce 10 independent
         # Dense layers, one for each time step.
@@ -72,7 +70,18 @@ class SimpleLstm:
         model.add(Dense(1, activation='sigmoid', input_shape = (0, lstm_layer_size)))
         # binary because we're doing binary classification (correct / incorrect
         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-        model.fit(X_train, y_train, validation_data=(X_train, y_train), epochs=3, batch_size=4)
+        model.fit(X_train, y_train, validation_data=(X_train, y_train), epochs=10, batch_size=100)
         Y_pred = model.predict(X_train)
-        print("prediction: ", Y_pred)
+        #print("prediction: ", Y_pred)
         print(model.summary())
+
+
+def data_in_time(data_X, data_y, time):
+    list = []
+    for i in range(len(data_X) - time + 1):
+        list.append(data_X[i:i + time])
+
+    data_X = np.reshape( np.concatenate(list), (data_X.shape[0] - time + 1, time, data_X.shape[1]))
+
+    data_y = data_y[time - 1:len(data_y)]
+    return data_X, data_y

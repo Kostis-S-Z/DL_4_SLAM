ee451c5b7f2af77b82d2417ba15f92302dc2fb47
Kostis-S-Z <kostis.s.z.24@gmail.com>
2019-05-09 19:26:52 +0200

    Build separate dataset files

--- starter_code/lstm.py
+++ starter_code/lstm.py
@@ -10,8 +10,8 @@ from keras.layers import Dense, Activation, Embedding, LSTM, TimeDistributed
 
 # Data evaluation functions
 import data
-from data import get_paths, load_data, write_predictions
-from preprocess_data import preprocess
+from data import get_paths, write_predictions
+from build_dataset import build_dataset
 from eval import evaluate
 
 get_paths()
@@ -24,11 +24,6 @@ pred_path = data.pred_path
 VERBOSE = 1  # 0 or 1
 KERAS_VERBOSE = 1  # 0 or 1
 
-# Data parameters
-MAX = 10000000  # Placeholder value to work as an on/off if statement
-TRAINING_PERC = 0.001  # Control how much (%) of the training data to actually use for training
-TEST_PERC = 0.05
-
 # FEATURES_TO_USE = ['user']  # 2595
 # FEATURES_TO_USE = ['countries']  # 66
 # FEATURES_TO_USE = ['client']  # 5
@@ -36,13 +31,18 @@ TEST_PERC = 0.05
 # FEATURES_TO_USE = ['format']  # 5
 # FEATURES_TO_USE = ['token']  # 2228
 
-FEATURES_TO_USE = ['user', 'countries', 'client', 'session', 'format', 'token']
+FEATURES_TO_USE = ['countries', 'client', 'session', 'format', 'token']
 THRESHOLD_OF_OCC = 2000
 
+# If you want to build a new data set with you features put preprocessed_data_id = ""
+# If you don't want to build new data and want to use existing preprocess, put their path here
+preprocessed_data_id = "9_5_18:0"
 
 # Model parameters
 now = datetime.datetime.now()
 MODEL_ID = str(now.day) + "_" + str(now.month) + "_" + str(now.hour) + ":" + str(now.minute)
+
+trained_model = None
 
 # Define the number of nodes in each layer, the last one is the output
 net_architecture = {
@@ -51,10 +51,10 @@ net_architecture = {
 }
 
 model_params = {
-    "batch_size": 100,  # number of samples in a batch
+    "batch_size": 64,  # number of samples in a batch
     "lr": 0.01,  # learning rate
     "epochs": 10,  # number of epochs
-    "time_steps": 50  # how many time steps to look back to
+    "time_steps": 100  # how many time steps to look back to
 }
 
 # use word embedding
@@ -71,6 +71,9 @@ model_params = {
 
 
 def main():
+    if not preprocessed_data_id:
+        build_dataset(MODEL_ID, train_path, test_path, model_params["time_steps"], FEATURES_TO_USE, THRESHOLD_OF_OCC)
+
     predictions = run_lstm()
 
     write_predictions(predictions)
@@ -88,64 +91,46 @@ def run_lstm():
     The chunks are split evenly, except the last one. The last one will contain a bit more.
     e.g when split 15% the last batch will contain ~200.000 exercises where as the others ~125.000
     """
-    num_chunks = int(1 / TRAINING_PERC)
-    # num_chunks = 2  # DEBUG: use if you want to test a really small part of the data
 
-    start_line = 0
-    total_instances = 0
-    total_exercises = 0
+    if not preprocessed_data_id:
+        data_id = MODEL_ID
+    else:
+        data_id = preprocessed_data_id
 
+    # Training
+    train_data = load_new_data(data_id, "train_data")
+    train_labels = load_new_data(data_id, "train_labels")
+    # train_id = load_new_data(data_id, "train_id")  # Do we need the training id?
     lstm_model = SimpleLSTM(net_architecture, **model_params)
+    lstm_model.train(train_data, train_labels)
+    lstm_model.save_model()
 
-    for chunk in range(num_chunks - 1):
-        # If in the last chunk, use all of the data left
-        if chunk != num_chunks - 1:
-            end_line = 0
-        else:
-            end_line = MAX  # the reader will read until the end of file and will exit
+    # Testing
 
-        training_data, training_labels, end_line, num_instance, num_exercises = load_data(train_path,
-                                                                                          perc_data_use=TRAINING_PERC,
-                                                                                          start_from_line=start_line,
-                                                                                          end_line=end_line)
-
-        training_data, training_labels, train_id = preprocess(model_params["time_steps"],
-                                                              training_data,
-                                                              FEATURES_TO_USE,
-                                                              THRESHOLD_OF_OCC,
-                                                              labels_dict=training_labels)
-
-        if VERBOSE > 0:
-            print("-- Training chunk {} / {} for {} instances, {} timesteps and "
-                  "{} features--".format(chunk + 1, num_chunks - 1, training_data.shape[0],
-                                         training_data.shape[1], training_data.shape[2]))
-
-        # If its the first chunk then you haven't trained a model yet and start from scratch
-        # otherwise resume from an already saved one
-        if chunk != 0:
-            trained_model = lstm_model.load_model()
-        else:
-            trained_model = None
-
-        lstm_model.train(training_data, training_labels, model=trained_model)
-        lstm_model.save_model()
-
-        # Make the ending line of this batch, the starting point of the next batch
-        start_line = end_line
-        total_instances += num_instance - model_params["time_steps"] + 1
-        total_exercises += num_exercises
-
-    if VERBOSE > 1:
-        print("total trained instances: {} total exercises: {}".format(total_instances, total_exercises))
-        print("\n-- Testing --\n")
-
-    test_data = load_data(test_path, perc_data_use=TEST_PERC)  # Load the test dataset
-
-    test_data, _, test_id = preprocess(model_params["time_steps"], test_data, FEATURES_TO_USE, THRESHOLD_OF_OCC)
+    test_data = load_new_data(data_id, "test_data")
+    # TODO: fix the ID thing being larger than the data
+    # if you fix it, there also needs to be a change in the code in the lstm.predict() function where the IDs are used
+    test_id = load_new_data(data_id, "test_id")
 
     predictions = lstm_model.predict(test_data, test_id)
 
     return predictions
+
+
+def load_new_data(data_id, data_type):
+
+    import pickle
+    with open("new_data/data_" + data_id + "/" + data_type + "_chunk_0", 'rb') as fr:
+        try:
+            data_chunk = pickle.load(fr)
+            if isinstance(data_chunk, (list,)):
+                print("Loaded {} with {} length".format(data_type, len(data_chunk)))
+            else:
+                print("Loaded {} with {} shape".format(data_type, data_chunk.shape))
+            return data_chunk
+        except IOError:
+            print("No such file")
+            exit()
 
 
 def write_results(results):
@@ -170,10 +155,10 @@ class SimpleLSTM:
         Initialize Neural Network with data and parameters
         """
         var_defaults = {
-            "batch_size": 10,  # number of samples in a batch
+            "batch_size": 64,  # number of samples in a batch
             "lr": 0.01,  # learning rate
             "epochs": 10,  # number of epochs
-            "time_steps": 5  # how many time steps to look back to
+            "time_steps": 50  # how many time steps to look back to
         }
 
         for var, default in var_defaults.items():
@@ -197,8 +182,6 @@ class SimpleLSTM:
         hidden_0 = self.net_architecture[0]
         # hidden_1 = self.net_architecture[1]
         # hidden_2 = self.net_architecture[2]
-        # hidden_3 = self.net_architecture[3]
-        # hidden_4 = self.net_architecture[4]
 
         output = self.net_architecture[1]
 
@@ -208,24 +191,7 @@ class SimpleLSTM:
         # model.add(LSTM(hidden_1, return_sequences=False))
         # model.add(LSTM(hidden_2, return_sequences=False))
 
-        # TODO: return sequence: true or false?
-        # TODO: Use BatchNormalization
-        # TODO: Use Dropout
-        # model.add(LSTM(hidden_1, return_sequences=True))
-        # model.add(LSTM(hidden_2, return_sequences=True))
-        # model.add(LSTM(hidden_3, return_sequences=True))
-        # model.add(LSTM(hidden_4, return_sequences=True))
-
         model.add(Dense(output, activation='sigmoid'))
-        """
-        About BCE + sigmoid:
-        As the loss function is central to learning, this means that a model employing last-layer sigmoid + BCE cannot 
-        discriminate among samples whose predicted class is either in extreme 
-        accordance or extreme discordance with their labels.
-        https://towardsdatascience.com/sigmoid-activation-and-binary-crossentropy-a-less-than-perfect-match-b801e130e31
-
-        so we can even try without any activation in the output layer and compute BCE from the raw outputs
-        """
 
         if KERAS_VERBOSE > 0:
             print(model.summary())
@@ -240,20 +206,20 @@ class SimpleLSTM:
         y_train = np.array(y_train)
 
         self.input_shape = x_train.shape[2]
+
         if model is None:
             model = self.init_model()
         else:
             print("Loading pre-existing model...")
             model = self.load_model()
 
-        # binary because we're doing binary classification (correct / incorrect
+        # binary because we're doing binary classification (correct / incorrect)
         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
         # Fit the training data to the model
-        model.fit(x_train, y_train,
-                  shuffle=False,
-                  epochs=self.epochs,
-                  batch_size=self.batch_size,
-                  verbose=KERAS_VERBOSE)
+        # Also use a part of the data for validation
+        model.fit(x_train, y_train, shuffle=False, epochs=self.epochs, validation_split=0.1,
+                  batch_size=self.batch_size, verbose=KERAS_VERBOSE)
 
         # save the model to a class variable for further use afterwards (only reading from this variable, no changing)
         self.model = model

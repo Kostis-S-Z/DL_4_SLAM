e4ce3cf758c5d4fa497e28023ab9fe385f8115f9
juliahen96 <julix474@gmail.com>
2019-05-14 00:02:17 +0200

    added function to save model parameters

--- starter_code/experiments.py
+++ starter_code/experiments.py
@@ -31,18 +31,38 @@ def experiment_0():
     use_pre_processed_data = False
     use_pre_processed_data = False
     model_params['epochs'] = 1
-
+    save_model(MODEL_ID)
     main()
 
 
-def save_model(self, model_id):
+def save_model(model_id):
     """
     Save current model with weights
     """
     if not os.path.exists("models/"):
         os.makedirs("models/")
-
-    self.model.save("models/parameters_" + model_id)
+    with open("models/parameters_" + model_id, "a+") as f:
+        f.write("---- Parameters of Model " + MODEL_ID + " ----\n")
+        f.write("    -------------------------------------------------------------\n")
+        for k in (model_params.keys()):
+            f.write("    {:<15} {:<15}\n".format(k, model_params[k]))
+        f.write("    -------------------------------------------------------------\n")
+        f.write("    {:<35} {:<15}\n".format('--Features_to_use-', ''))
+        f.write("    ")
+        for k in FEATURES_TO_USE[0:-1]:
+            f.write(k + ", ")
+        f.write(FEATURES_TO_USE[-1] + "\n")
+        f.write("    threshold " + str(THRESHOLD_OF_OCC) + "\n")
+        f.write("    -------------------------------------------------------------\n")
+        f.write("    {:<25} {:<15}\n".format('--net_architechture-','' ))
+        for k in sorted(net_architecture.keys()):
+            f.write("    {:<15} {:<15}\n".format(k, net_architecture[k]))
+        f.write("    -------------------------------------------------------------\n")
+        f.write("    {:<25} {:<15}\n".format('--class_weights-', ''))
+        for k in sorted(class_weight.keys()):
+            f.write("    {:<15} {:<15}\n".format(k, int(class_weight[k])))
+        f.write("    -------------------------------------------------------------\n\n")
+        f.close()
 
 
 # spexify which experiment you want to run

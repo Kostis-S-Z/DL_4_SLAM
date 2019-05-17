c51637cc1973b44dc9ce780e7a81e5eb5ac000ff
juliahen96 <julix474@gmail.com>
2019-05-17 13:03:50 +0200

    more data

--- starter_code/build_dataset.py
+++ starter_code/build_dataset.py
@@ -19,9 +19,9 @@ if DEBUG:
     AMOUNT_DATA_USE = 0.001
 else:
     # control how much data to use in one chunk in cloud # max 0.025
-    PERC_OF_DATA_PER_CHUNK = 0.001#025
+    PERC_OF_DATA_PER_CHUNK = 0.004#025
     # control how much data use in total
-    AMOUNT_DATA_USE = 0.001
+    AMOUNT_DATA_USE = 0.004
 
 # compute how many chunks we get based on how much data we want to use in total and how much data we can use in one chunk
 NUM_CHUNK_FILES = int(AMOUNT_DATA_USE / PERC_OF_DATA_PER_CHUNK)
